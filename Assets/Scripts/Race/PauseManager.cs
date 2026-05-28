47ea98d1738c02c15ba65544223f6b76905cb4e8
foryasith <yasithprashan@gmail.com>
2026-05-29 02:27:14 +0530

    feat(gv): add pause system with P key and pause panel UI

---
+++ Assets/Scripts/Race/PauseManager.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using TMPro;
+
+public class PauseManager : MonoBehaviour
+{
+    [Header("UI Reference")]
+    public GameObject pausePanel;
+
+    private bool isPaused = false;
+
+    public static PauseManager Instance;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        // Make sure pause panel is hidden at start
+        if (pausePanel != null)
+            pausePanel.SetActive(false);
+    }
+
+    void Update()
+    {
+        // Press P to pause/unpause
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            TogglePause();
+        }
+    }
+
+    public void TogglePause()
+    {
+        isPaused = !isPaused;
+
+        // Time.timeScale 0 = frozen, 1 = normal
+        Time.timeScale = isPaused ? 0f : 1f;
+
+        if (pausePanel != null)
+            pausePanel.SetActive(isPaused);
+
+        Debug.Log(isPaused ? "Game Paused" :
+            "Game Resumed");
+    }
+}
